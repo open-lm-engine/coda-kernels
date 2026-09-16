347020f198b3eb1691c956b50146b609e4c464a4
Han Guo <alex.guohan1106@gmail.com>
2026-09-16 16:29:24 -0400

    minor

--- coda/kernels/functional/swiglu.py
+++ coda/kernels/functional/swiglu.py
@@ -11,16 +11,16 @@ class LinearSwiGLU(torch.autograd.Function):
     @input_guard
     @autocast_custom_fwd
     def forward(ctx, x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
-        pre_act, out = gemm_swiglu(x, weight.mT)
-        ctx.save_for_backward(x, weight, pre_act)
+        preact, out = gemm_swiglu(x, weight.mT)
+        ctx.save_for_backward(x, weight, preact)
         return out
 
     @staticmethod
     @input_guard
     @autocast_custom_bwd
     def backward(ctx, dout: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        x, weight, pre_act = ctx.saved_tensors
-        grad_pre = dswiglu_backward(pre_act, dout)
+        x, weight, preact = ctx.saved_tensors
+        grad_pre = dswiglu_backward(preact, dout)
         dx = gemm(grad_pre, weight)
         dweight = gemm(grad_pre.mT, x)
         return dx, dweight
