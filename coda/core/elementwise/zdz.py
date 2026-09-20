f1390ad580e88d4d2c72af3d56143d2884a2fa8c
Han Guo <alex.guohan1106@gmail.com>
2026-09-19 23:08:35 -0400

    update

--- coda/core/elementwise/zdz.py
+++ coda/core/elementwise/zdz.py
@@ -16,6 +16,73 @@ from coda.core.ops import memory_utils
 from coda.core.ops import creation_utils
 
 
+@jit_cache
+def _compile_rope_bwd_zdz(
+    size: int,
+    scale: float,
+    dtype: type[cute.Numeric],
+    pos_dtype: type[cute.Numeric],
+    freq_dtype: type[cute.Numeric],
+    thr_m: int,
+    thr_n: int,
+    val_m: int,
+) -> Callable:
+    m = cute.sym_int()
+    vector_size = cutlass.const_expr(constants.NUM_BITS_PER_COPY // dtype.width)
+    mY = cute.runtime.make_fake_tensor(
+        dtype=dtype,
+        shape=(m, size),
+        stride=(cute.sym_int64(divisibility=vector_size), 1),
+        assumed_align=16,
+    )
+    mDY = cute.runtime.make_fake_tensor(
+        dtype=dtype,
+        shape=(m, size),
+        stride=(cute.sym_int64(divisibility=vector_size), 1),
+        assumed_align=16,
+    )
+    mDZ = cute.runtime.make_fake_tensor(
+        dtype=dtype,
+        shape=(m, size),
+        stride=(cute.sym_int64(divisibility=vector_size), 1),
+        assumed_align=16,
+    )
+    mZdZ = cute.runtime.make_fake_tensor(
+        dtype=cute.Float32,
+        shape=(m,),
+        stride=(1,),
+        assumed_align=4,
+    )
+    mPos = cute.runtime.make_fake_tensor(
+        dtype=pos_dtype,
+        shape=(m,),
+        stride=(1,),
+        assumed_align=pos_dtype.width // 8,
+    )
+    mFreq = cute.runtime.make_fake_tensor(
+        dtype=freq_dtype,
+        shape=(size,),
+        stride=(1,),
+        assumed_align=freq_dtype.width // 8,
+    )
+    stream = cute.runtime.make_fake_stream(use_tvm_ffi_env_stream=True)
+    return cute.compile(
+        _rope_bwd_zdz,
+        mY=mY,
+        mDY=mDY,
+        mDZ=mDZ,
+        mZdZ=mZdZ,
+        mPos=mPos,
+        mFreq=mFreq,
+        scale=scale,
+        thr_m=thr_m,
+        thr_n=thr_n,
+        val_m=val_m,
+        stream=stream,
+        options="--enable-tvm-ffi",
+    )
+
+
 def rope_bwd_zdz_(
     y: torch.Tensor,
     dy: torch.Tensor,
